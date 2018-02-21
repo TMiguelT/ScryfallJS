58b019fb6c02186cb78c731c9f371df1d49d7d7d
Michael Milton <ttmigueltt@gmail.com>
2018-02-22 00:01:44 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+require('url');
+
+const API_ROOT = 'https://api.scryfall.com';
+
+class ScryfallApiClient {
+    constructor(){
+
+    }
+}
